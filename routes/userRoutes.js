af7cf1afb830a18521dd3be1050d6306c45e5089
Sannya Wasim <sannya.wasim01@gmail.com>
2023-02-26 18:46:44 +0500

    Updated data in routes and controller files

---
+++ routes/userRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+// importing user route
+userRoute = require('./controllers/usersController.js');
+
+// mapping user controller onto user route
+router.get("/", userRoute.userController)
+
+/*
+router.get("/", (req, res)=>{
+    res.send({data : "Here is your data"});
+})
+
+router.post("/", (req, res)=>{
+    res.send({data : "User created!"})
+})
+
+router.put("/", (req, res)=>{
+    res.send({data : "User updated!"})
+})
+
+router.delete('/', (req, res)=>{
+    res.send({data : "User deleted!"})
+})
+*/
+
+// exporting user route
+module.exports = router;
